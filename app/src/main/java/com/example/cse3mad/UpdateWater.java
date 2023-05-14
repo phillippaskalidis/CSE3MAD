7aa981778326cf0f5d2eb4fce311c24f45e3f395
Layla Ashour <21057599@students.latrobe.edu.au>
2023-05-14 17:41:50 +1000

    added activity_update.xml and activity_update_water.xml

--- app/src/main/java/com/example/cse3mad/UpdateWater.java
+++ app/src/main/java/com/example/cse3mad/UpdateWater.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UpdateCalorie extends AppCompatActivity {
+public class UpdateWater extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_calorie);
+        setContentView(R.layout.activity_update_water);
     }
 }
