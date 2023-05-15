4ad09be12bdf6e09bf7914cbcfdacdca5ed02a18
Layla Ashour <21057599@students.latrobe.edu.au>
2023-05-16 02:29:42 +1000

    added some methods to CaloriesUpdate Activity

--- app/src/main/java/com/example/cse3mad/UpdateCalorie.java
+++ app/src/main/java/com/example/cse3mad/UpdateCalorie.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class UpdateCalorie extends AppCompatActivity {
@@ -15,8 +16,13 @@ public class UpdateCalorie extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_calorie);
 
+        // declaring variables & Buttons and setting them to the relevant views by Id
         Button doneBtn = findViewById(R.id.Done_Button);
         Button cancelBtn = findViewById(R.id.Cancel_Button);
+        EditText ETTotalCalories = (EditText) findViewById(R.id.eTTotalCal);
+        EditText ETProtein = (EditText) findViewById(R.id.eTProtein);
+        EditText ETFat = (EditText) findViewById(R.id.eTFat);
+        EditText ETCarbs = (EditText) findViewById(R.id.eTCarbs);
 
 
         //doneBtn will save all the changes made to then display on first activity
@@ -25,11 +31,27 @@ public class UpdateCalorie extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
+
+
+                //do some calculations
+
+
+                //save the new calculated variable into the variable in the main
+
+
+
                 //Toast will let the user know when the changes have been updated
                 Toast.makeText(UpdateCalorie.this, " Calories updated! ", Toast.LENGTH_SHORT).show();
             }
+
         });
 
+
+    }
+
+    //Method to calculate the macros of each meal that will then
+    public void CalculateMacros(String MealCalories, String MealCarbs, String MealProtein, String MealFat)
+    {
 
     }
     }
