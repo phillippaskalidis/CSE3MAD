3eb508865898ee9b222125ad33d3622cb7b85c68
Layla Ashour <21057599@students.latrobe.edu.au>
2023-05-15 11:44:16 +1000

    Updated Calories Update Activity, fixed the XML code

--- app/src/main/java/com/example/cse3mad/UpdateCalorie.java
+++ app/src/main/java/com/example/cse3mad/UpdateCalorie.java
@@ -25,10 +25,11 @@ public class UpdateCalorie extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-
                 //Toast will let the user know when the changes have been updated
                 Toast.makeText(UpdateCalorie.this, " Calories updated! ", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
     }
