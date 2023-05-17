9b000ffd179775ba32a47bcb0f8a29df485f5f2f
Layla Ashour <21057599@students.latrobe.edu.au>
2023-05-17 14:37:05 +1000

    added some methods to CaloriesUpdate Activity

--- app/src/main/java/com/example/cse3mad/UpdateCalorie.java
+++ app/src/main/java/com/example/cse3mad/UpdateCalorie.java
@@ -11,6 +11,37 @@ import android.widget.Toast;
 
 public class UpdateCalorie extends AppCompatActivity {
 
+    public void CalculateMacros(int MealCalories, int MealCarbs, int MealProtein, int MealFat)
+    {
+        //Getting the ratio from each category from the meal
+        int ProteinRatio = MealProtein/100;
+        int FatRatio = MealFat/100;
+        int CarbsRatio = MealCarbs/100;
+
+
+        // Get macro total from the meal
+        int PResult = MealCalories * ProteinRatio;
+        int FResult = MealCalories * FatRatio;
+        int CResult = MealCalories * CarbsRatio;
+
+
+        // this will be assigned to the variable of another activity but will save in this for now
+        int RequiredProtein = 0;
+        int RequiredFat = 0;
+        int RequiredCarbs = 0;
+        int RequiredCalories =0;
+
+        // minus the macro from that meal from the required macro amount for that day
+        int Protein = RequiredProtein - PResult;
+        int Fat = RequiredFat - FResult;
+        int Carbs = RequiredCarbs - CResult;
+        int Calories = RequiredCalories - MealCalories;
+
+
+
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +55,9 @@ public class UpdateCalorie extends AppCompatActivity {
         EditText ETFat = (EditText) findViewById(R.id.eTFat);
         EditText ETCarbs = (EditText) findViewById(R.id.eTCarbs);
 
+        //convert ET to Int to use in method
+
+        int PInput;
 
         //doneBtn will save all the changes made to then display on first activity
         doneBtn.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +67,9 @@ public class UpdateCalorie extends AppCompatActivity {
             {
 
 
-                //do some calculations
+
+
+
 
 
                 //save the new calculated variable into the variable in the main
@@ -50,8 +86,5 @@ public class UpdateCalorie extends AppCompatActivity {
     }
 
     //Method to calculate the macros of each meal that will then
-    public void CalculateMacros(String MealCalories, String MealCarbs, String MealProtein, String MealFat)
-    {
 
-    }
     }
