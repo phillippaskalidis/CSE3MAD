16798996ddc40f3ee9dc357e94f188786695fe18
Layla Ashour <21057599@students.latrobe.edu.au>
2023-05-20 14:54:43 +1000

    finished CaloriesUpdate Activity

--- app/src/main/java/com/example/cse3mad/UpdateCalorie.java
+++ app/src/main/java/com/example/cse3mad/UpdateCalorie.java
@@ -11,6 +11,14 @@ import android.widget.Toast;
 
 public class UpdateCalorie extends AppCompatActivity {
 
+    Button doneBtn , cancelBtn;
+    EditText ETCalo , ETProtein , ETCarbs, ETFat;
+    String caloriesValue , proteinValue , carbsValue, fatValue;
+    // Input values that will go into method
+    int CaloriesInput, PInput , CInput, FInput;
+
+
+    //Method to calculate the macros of each meal that will then
     public void CalculateMacros(int MealCalories, int MealCarbs, int MealProtein, int MealFat)
     {
         //Getting the ratio from each category from the meal
@@ -18,12 +26,10 @@ public class UpdateCalorie extends AppCompatActivity {
         int FatRatio = MealFat/100;
         int CarbsRatio = MealCarbs/100;
 
-
         // Get macro total from the meal
         int PResult = MealCalories * ProteinRatio;
         int FResult = MealCalories * FatRatio;
         int CResult = MealCalories * CarbsRatio;
-
 
         // this will be assigned to the variable of another activity but will save in this for now
         int RequiredProtein = 0;
@@ -37,10 +43,6 @@ public class UpdateCalorie extends AppCompatActivity {
         int Carbs = RequiredCarbs - CResult;
         int Calories = RequiredCalories - MealCalories;
 
-
-
-
-
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,43 +50,39 @@ public class UpdateCalorie extends AppCompatActivity {
         setContentView(R.layout.activity_update_calorie);
 
         // declaring variables & Buttons and setting them to the relevant views by Id
-        Button doneBtn = findViewById(R.id.Done_Button);
-        Button cancelBtn = findViewById(R.id.Cancel_Button);
-        EditText ETTotalCalories = (EditText) findViewById(R.id.eTTotalCal);
-        EditText ETProtein = (EditText) findViewById(R.id.eTProtein);
-        EditText ETFat = (EditText) findViewById(R.id.eTFat);
-        EditText ETCarbs = (EditText) findViewById(R.id.eTCarbs);
+        doneBtn = findViewById(R.id.Done_Button);
+        cancelBtn = findViewById(R.id.Cancel_Button);
 
-        //convert ET to Int to use in method
 
-        int PInput;
+        ETCalo = (EditText) findViewById(R.id.CaloriesInput);
+        ETProtein = (EditText) findViewById(R.id.PInput);
+        ETFat = (EditText) findViewById(R.id.CInput);
+        ETCarbs = (EditText) findViewById(R.id.FInput);
+
+        //get text from edit text
+        caloriesValue  = ETCalo.getText().toString();
+        proteinValue = ETProtein.getText().toString();
+        carbsValue = ETCarbs.getText().toString();
+        fatValue = ETFat.getText().toString();
+
+        //turn the string to an Int to then use in the method to calculate the macros of that meal
+        CaloriesInput = Integer.parseInt(caloriesValue);
+        PInput  = Integer.parseInt(proteinValue);
+        CInput = Integer.parseInt(carbsValue);
+        FInput = Integer.parseInt(fatValue);
 
         //doneBtn will save all the changes made to then display on first activity
         doneBtn.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view)
             {
-
-
-
-
-
-
-
-                //save the new calculated variable into the variable in the main
-
-
-
+                CalculateMacros(CaloriesInput, CInput, PInput,FInput);
                 //Toast will let the user know when the changes have been updated
                 Toast.makeText(UpdateCalorie.this, " Calories updated! ", Toast.LENGTH_SHORT).show();
             }
 
         });
 
-
     }
-
-    //Method to calculate the macros of each meal that will then
 
     }
