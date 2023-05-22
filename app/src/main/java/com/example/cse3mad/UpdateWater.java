0b1afc885fcb5ec446d2043e26c59553c702737d
phillippaskalidis <phillip.paskalidis@gmail.com>
2023-05-22 12:54:58 +1000

    updatewater dropdown box

--- app/src/main/java/com/example/cse3mad/UpdateWater.java
+++ app/src/main/java/com/example/cse3mad/UpdateWater.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class UpdateWater extends AppCompatActivity {
 
     private Button doneBtn, cancelBtn;
     private EditText ETWater;
+    private Spinner waterSpinner;
     private String waterValue;
 
     @Override
@@ -23,6 +27,35 @@ public class UpdateWater extends AppCompatActivity {
         doneBtn = findViewById(R.id.Done_Button);
         cancelBtn = findViewById(R.id.Cancel_Button);
         ETWater = findViewById(R.id.WaterInput);
+        waterSpinner = findViewById(R.id.waterSpinner);
+
+        // Define the options for the spinner
+        String[] waterOptions = {"500ml", "1L", "1.5L", "2L"};
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, waterOptions);
+
+        // Set the layout style for the dropdown
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Set the adapter to the spinner
+        waterSpinner.setAdapter(adapter);
+
+        // Set the spinner item selection listener
+        waterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                // Get the selected water option
+                String selectedWater = waterOptions[position];
+
+                // You can perform any additional actions based on the selected item here
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Handle the case where no item is selected
+            }
+        });
 
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +77,21 @@ public class UpdateWater extends AppCompatActivity {
 
                 try {
                     double waterIntake = Double.parseDouble(waterValue.trim());
+
+                    // Get the selected water option
+                    String selectedWater = waterSpinner.getSelectedItem().toString();
+
+                    // Update the waterIntake based on the selectedWater
+                    if (selectedWater.equals("500 ml")) {
+                        waterIntake = 0.5;
+                    } else if (selectedWater.equals("1L")) {
+                        waterIntake = 1.0;
+                    } else if (selectedWater.equals("1.5L")) {
+                        waterIntake = 1.5;
+                    } else if (selectedWater.equals("2L")) {
+                        waterIntake = 2.0;
+                    }
+
                     Intent updateWaterIntent = new Intent(UpdateWater.this, HomeActivity.class);
                     updateWaterIntent.putExtra("WaterIntake", waterIntake);
                     startActivity(updateWaterIntent);
